3535710280e6d3c311228fc75be72f33f439f2b5
Christyan Simbolon <sambungdata@gmail.com>
2026-04-15 22:21:29 +0700

    feat-backend: update main app

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,7 @@
 from app.core.database import Base, engine
 from app.modules.auth.api.routes import router as auth_router
 from app.modules.users.api.routes import router as users_router
-from app.modules.users.infrastructure.models import UserModel
+from app.modules.users.infrastructure.models import User
 from fastapi import FastAPI
 
 app = FastAPI()
