faefe23ee37045d8f8da559b1012f9c0288145a4
Christyan Simbolon <sambungdata@gmail.com>
2026-04-16 20:44:46 +0700

    feat-backend: update users schemas

--- backend/app/modules/users/application/schemas.py
+++ backend/app/modules/users/application/schemas.py
@@ -1,13 +1,23 @@
+# app/modules/users/application/schemas.py
 from datetime import datetime
 from uuid import UUID
 
 from pydantic import BaseModel, EmailStr
 
 
-class CreateUserRequest(BaseModel):
+class RegisterRequest(BaseModel):
     email: EmailStr
     password: str
 
+
+class LoginRequest(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class TokenResponse(BaseModel):
+    access_token: str
+    token_type: str = "bearer"
 
 class UserResponse(BaseModel):
     id: UUID
