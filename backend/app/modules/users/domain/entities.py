1fcf7ebeb9cc9e92c271db1571ca54f1ef8baa19
Christyan Simbolon <sambungdata@gmail.com>
2026-04-16 20:47:27 +0700

    feat-backend: update users entities

--- backend/app/modules/users/domain/entities.py
+++ backend/app/modules/users/domain/entities.py
@@ -1,22 +1,28 @@
-from dataclasses import dataclass
+# modules/users/domain/entities.py
+import uuid
 from datetime import datetime
-from uuid import UUID, uuid4
 
 
-@dataclass
 class User:
-    id: UUID
-    email: str
-    hashed_password: str
-    is_active: bool
-    created_at: datetime
+    def __init__(
+        self,
+        id,
+        email,
+        hashed_password,
+        is_active=True,
+        created_at=None,
+    ):
+        self.id = id
+        self.email = email
+        self.hashed_password = hashed_password
+        self.is_active = is_active
+        self.created_at = created_at or datetime.utcnow()
 
-    @staticmethod
-    def create(email: str, hashed_password: str) -> "User":
-        return User(
-            id=uuid4(),
+    @classmethod
+    def create(cls, email: str, hashed_password: str):
+        return cls(
+            id=uuid.uuid4(),
             email=email,
             hashed_password=hashed_password,
             is_active=True,
-            created_at=datetime.utcnow(),
         )
