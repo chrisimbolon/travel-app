c2e6218c5174065622f4b1ed07db570a6cb45767
Christyan Simbolon <sambungdata@gmail.com>
2026-04-16 21:50:09 +0700

    feat-backend: update main app

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,8 @@
-# app/main.py
 from contextlib import asynccontextmanager
 
 from app.core.database import Base, engine
 from app.modules.auth.api.routes import router as auth_router
+from app.modules.trips.api.routes import router as trips_router
 from fastapi import FastAPI
 
 
@@ -15,3 +15,4 @@ async def lifespan(app: FastAPI):
 app = FastAPI(title="Transitku API", lifespan=lifespan)
 
 app.include_router(auth_router)
+app.include_router(trips_router)
