a01a7605f074b38178fa4d978b33c4c618e3d09f
Christyan Simbolon <sambungdata@gmail.com>
2026-04-17 21:48:51 +0700

    feat-backeand: update main app

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,11 @@
 from contextlib import asynccontextmanager
 
+from app.core.config import settings
 from app.core.database import Base, engine
 from app.modules.auth.api.routes import router as auth_router
 from app.modules.trips.api.routes import router as trips_router
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 
 @asynccontextmanager
@@ -13,6 +15,14 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(title="Transitku API", lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.CORS_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth_router)
 app.include_router(trips_router)
